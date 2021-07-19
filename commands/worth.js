93beaf121262919a7745a876261bbebe1bda1e84
rajdeep123von <59657573+rajdeep123von@users.noreply.github.com>
2021-07-19 13:41:23 +0530

    Delete commands directory

--- commands/worth.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    name: 'worth',
-    description : "this is a ping command!",
-    execute(client, message, args, Discord){
-        message.channel.send('The current worth of Dikcorps is $999,999,112,440,068')
-    }
-}
